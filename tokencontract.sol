862354f18e844dccd3ecf11891bde4500fbe609a
archive49 <79701139+archive49@users.noreply.github.com>
2022-07-29 15:38:50 +0600

    Create tokencontract.sol

---
+++ tokencontract.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract TheTokenName is ERC721Enumerable, Ownable {
+
+    using Strings for uint256;
+
+    string _baseTokenURI;
+    uint256 private _reserved = 100;
+    uint256 private _price = 0.06 ether;
+    bool public _paused = true;
+
+    // withdraw addresses
+    address t1 = 0xb0;
+    address t2 = 0xb1;
+    address t3 = 0xb2;
+    address t4 = 0xb3;
+
+    // 9999 tokens in total
+    constructor(string memory baseURI) ERC721("The Token", "TOKN")  {
+        setBaseURI(baseURI);
+
+        // team gets the first 4 tokens
+        _safeMint( t1, 0);
+        _safeMint( t2, 1);
+        _safeMint( t3, 2);
+        _safeMint( t4, 3);
+    }
+
+    function adopt(uint256 num) public payable {
+        uint256 supply = totalSupply();
+        require( !_paused,                              "Sale paused" );
+        require( num < 21,                              "You can adopt a maximum of 20 tokens" );
+        require( supply + num < 10000 - _reserved,      "Exceeds maximum token supply" );
+        require( msg.value >= _price * num,             "Ether sent is not correct" );
+
+        for(uint256 i; i < num; i++){
+            _safeMint( msg.sender, supply + i );
+        }
+    }
+
+    function walletOfOwner(address _owner) public view returns(uint256[] memory) {
+        uint256 tokenCount = balanceOf(_owner);
+
+        uint256[] memory tokensId = new uint256[](tokenCount);
+        for(uint256 i; i < tokenCount; i++){
+            tokensId[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+        return tokensId;
+    }
+
+    // changing price in case eth goes boom
+    function setPrice(uint256 _newPrice) public onlyOwner() {
+        _price = _newPrice;
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function setBaseURI(string memory baseURI) public onlyOwner {
+        _baseTokenURI = baseURI;
+    }
+
+    function getPrice() public view returns (uint256){
+        return _price;
+    }
+
+    function giveAway(address _to, uint256 _amount) external onlyOwner() {
+        require( _amount <= _reserved, "Exceeds reserved token supply" );
+
+        uint256 supply = totalSupply();
+        for(uint256 i; i < _amount; i++){
+            _safeMint( _to, supply + i );
+        }
+
+        _reserved -= _amount;
+    }
+
+    function pause(bool val) public onlyOwner {
+        _paused = val;
+    }
+
+    function withdrawAll() public payable onlyOwner {
+        uint256 _each = address(this).balance / 4;
+        require(payable(t1).send(_each));
+        require(payable(t2).send(_each));
+        require(payable(t3).send(_each));
+        require(payable(t4).send(_each));
+    }
+}
